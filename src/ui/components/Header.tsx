41f5d4081767738e5552100f8a523ba94656963e
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2023-01-25 12:06:41 -0300

    first commit

---
+++ src/ui/components/Header.tsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return(
+        <header>
+            <nav className="bg-slate-100 w-full h-10">
+                <a href="#">Logo</a>
+                <ul>
+                    <li>Men</li>
+                    <li>Woman</li>
+                    <li>Kids</li>
+                </ul>
+                <ul>
+                    <li>Cart</li>
+                    <li>Heart</li>
+                </ul>
+            </nav>
+            <h1 className="text-gray-600">TESTE</h1>
+        </header>
+    )
+}
